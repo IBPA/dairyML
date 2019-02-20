408b9ddccd4db80458ddd063cdd26b75750589fa
g-simmons <simmons.gabe@gmail.com>
2019-02-20 13:36:51 -0800

    fixes to test script, pretrain model

--- src/test.py
+++ src/test.py
@@ -11,6 +11,7 @@ from xgboost import XGBRegressor, XGBClassifier
 from dairyml import XGBCombined
 
 from skll.metrics import spearman, pearson
+from sklearn.utils import shuffle
 from sklearn.model_selection import cross_validate, RepeatedKFold
 from sklearn.metrics import r2_score, mutual_info_score, make_scorer, mean_absolute_error
 from sklearn.preprocessing import StandardScaler
@@ -29,7 +30,6 @@ def main(argv):
 		print('Not enough arguments specified\n Usage: test.py <model path> <input file path>')
 		return
 	else: 
-
 		#Load the model
 		model_path = argv[0]
 		print('Loading model at {}'.format(model_path))
@@ -43,32 +43,22 @@ def main(argv):
 		data = data.set_index('FoodCode')
 		numerical_features = data.columns[1:-1]
 
-		# #shuffle??
-
 		#Scale the features to 0 mean and unit variance
 		print('Scaling input features...')
 		ss = StandardScaler()
 		X = pd.DataFrame(ss.fit_transform(data[numerical_features]),columns=data[numerical_features].columns,index=data.index)
 
-		#Remove outliers
-		print('Removing outliers...')
-		iso = IsolationForest(contamination=.013)
-		outliers = iso.fit_predict(X)
-		print('Outliers removed: {}'.format(sum(outliers == -1)))
-		X = X[outliers == 1]
+		Y = data['lac.per.100g']
 
-		#Target variable
-		Y = data['lac.per.100g'][outliers == 1]
-
-		#splitter for CV
-		splitter= RepeatedKFold(n_splits=N_SPLITS,n_repeats=N_REPEATS)
+		# shuffle the data
+		X, Y = shuffle(X,Y)
 
 		#scoring
-		scoring = {'r2':make_scorer(r2_score), 
-		   'SRC':make_scorer(spearman), 
-		   'PCC':make_scorer(pearson), 
-		   'MI':make_scorer(mutual_info_score), 
-		   'MAE':make_scorer(mean_absolute_error)}
+		scoring = {'r2':r2_score, 
+		   'SRC':spearman, 
+		   'PCC':pearson, 
+		   'MI':mutual_info_score, 
+		   'MAE':mean_absolute_error}
 
 		time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 
@@ -80,18 +70,20 @@ def main(argv):
 		if not os.path.exists(results_dir):
 			os.makedirs(results_dir)
 
-		print('Testing the model... ({}-fold CV, repeated {}x)'.format(N_SPLITS,N_REPEATS))
-		xgb_combined_results = cross_validate(model,X,Y,cv=splitter,scoring=scoring)
+		print('Testing the model... '.format(N_SPLITS,N_REPEATS))
+		# Predictions
+		Y_pred = model.predict(X)
 
-
+		#score the predictions
 		print('\nResults: ')
-		for score_name in scoring.keys():
-			print('{}: {}'.format(score_name,np.round(np.mean(xgb_combined_results['test_'+score_name]),2)))
-			overall_results.loc['XGB Combined',score_name] = np.round(np.mean(xgb_combined_results['test_'+score_name]),2)
-		
+		for name, metric in scoring.items():
+			score = np.round(metric(Y,Y_pred))
+			print('{}: {}'.format(name,score))
+			overall_results.loc['XGB Combined',name] = score
+
 		overall_results.to_csv(results_path)
 		print('\nResults saved to {}'.format(results_path))
-	return
+		return
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
