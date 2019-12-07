c7caaadf62d4a3057626ac149fdefaee0731f00c
Gabriel Simmons <simmons.gabe@gmail.com>
2019-12-07 13:38:22 -0800

    refactor test script, all models run through test script on training data

--- src/scoring.py
+++ src/scoring.py
@@ -1,5 +1,5 @@
 from mutual_info import mutual_info_regression
-from sklearn.metrics import r2_score, explained_variance_score, mean_absolute_error, make_scorer
+from sklearn.metrics import r2_score, explained_variance_score, mean_absolute_error, make_scorer, accuracy_score, f1_score
 from skll.metrics import spearman, pearson
 
 scoring = {'r2':make_scorer(r2_score), 
@@ -7,3 +7,12 @@ scoring = {'r2':make_scorer(r2_score),
            'PCC':make_scorer(lambda x,y: pearson(x.flatten(),y.flatten())), 
            'MI':make_scorer(mutual_info_regression), 
            'MAE':make_scorer(mean_absolute_error)}
+
+scoring_test = {'r2':r2_score, 
+           'SRC':spearman, 
+           'PCC':pearson, 
+           'MI': lambda x,y: mutual_info_regression(x.values.reshape(-1,1),y.values.reshape(-1,1))[0], 
+           'MAE':mean_absolute_error}
+
+scoring_clf = {'classifier_accuracy': accuracy_score, 
+           'classifier_f1': f1_score }
--- src/test.py
+++ src/test.py
@@ -13,14 +13,86 @@ from dairyml import XGBCombined
 from skll.metrics import spearman, pearson
 from sklearn.utils import shuffle
 from sklearn.model_selection import cross_validate, RepeatedKFold
-from sklearn.metrics import r2_score, mutual_info_score, make_scorer, mean_absolute_error, accuracy_score, f1_score
+from sklearn.metrics import r2_score, make_scorer, mean_absolute_error
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import IsolationForest
+from scoring import *
 
 
 
 import warnings
 warnings.filterwarnings("ignore")
+
+results_dir = '../reports/'
+if not os.path.exists(results_dir):
+    os.makedirs(results_dir)
+
+def my_load_model(model_path):
+    print('Loading model at {}'.format(model_path))
+    if 'ffnn' in model_path:
+        from tensorflow.keras.models import load_model
+        model = load_model(model_path)
+    else:
+        with open(model_path, "rb" ) as f:
+            model = pkl.load(f)
+
+    return model
+
+
+def scale_data(data):
+    print('Scaling input features...')
+    train_means = np.loadtxt('./scaling/train_feature_means.csv',delimiter=',')
+    train_vars = np.loadtxt('./scaling/train_feature_variances.csv',delimiter=',')
+    scaled_data = (data - train_means) / np.sqrt(train_vars)
+
+    return scaled_data
+
+def get_model_predictions(model,X):
+    # Get model predictions
+    print('Testing the model... ')
+    # full predictions
+    predictions = pd.DataFrame(index=X.index)
+
+    Y_pred = model.predict(X)
+
+    predictions['Y_pred'] = Y_pred
+    predictions['Y_pred_reg'] = np.nan
+    predictions['Y_pred_clas'] = np.nan
+    
+    try:
+        Y_pred_reg = model.reg.predict(X) # regressor predictions (combined models only)
+        predictions['Y_pred_reg'] = Y_pred_reg
+
+        Y_pred_clas = model.clas.predict(X) #classifier predictions (combined models only)
+        predictions['Y_pred_clas'] = Y_pred_clas
+        predictions['Y_pred_clas'] = predictions['Y_pred_clas'].astype(int)
+
+    except AttributeError:
+        pass
+
+    return predictions
+
+def score_predictions(predictions,model_name):
+    results = pd.DataFrame()
+    #score the predictions
+    print('\nResults: ')
+    for name, metric in scoring_test.items():
+        score = np.round(metric(predictions['Y'],predictions['Y_pred']),2)
+        print('{}: {}'.format(name,score))
+        results.loc[model_name,name] = score
+
+    if not all(np.isnan(predictions['Y_pred_clas'])):
+        for name, metric in scoring_clf.items():
+            score = np.round(metric(predictions['Y_binary'],predictions['Y_pred_clas']),2)
+            print('{}: {}'.format(name,score))
+            results.loc[model_name,name] = score
+    else:
+        for name, metric in scoring_clf.items():
+            score = np.nan
+            print('{}: {}'.format(name,score))
+            results.loc[model_name,name] = score
+
+    return results
 
 
 def main(argv):
@@ -31,13 +103,7 @@ def main(argv):
     else: 
         #Load the model, pretrained on the full training data
         model_path = argv[0]
-        print('Loading model at {}'.format(model_path))
-        if 'ffnn' in model_path:
-            from tensorflow.keras.models import load_model
-            model = load_model(model_path)
-        else:
-            with open(model_path, "rb" ) as f:
-                model = pkl.load(f)
+        model = my_load_model(model_path)
 
         #Load test data
         data_path = argv[1]
@@ -46,79 +112,20 @@ def main(argv):
         data = data.set_index('FoodCode')
         numerical_features = data.columns[1:-1]
 
-        # print(data[numerical_features])
-
-        #Scale the features to 0 mean and unit variance
-        print('Scaling input features...')
-        train_means = np.loadtxt('./scaling/train_feature_means.csv',delimiter=',')
-        train_vars = np.loadtxt('./scaling/train_feature_variances.csv',delimiter=',')
-
-        X = (data[numerical_features] - train_means) / np.sqrt(train_vars)
-
+        # scale the data
+        X = scale_data(data[numerical_features])
 
         # get the target variable
         Y = data['lac.per.100g']
-        Y_binary = (Y != 0)
 
-        #list scoring measure names and functions
-        scoring_full = {'r2':r2_score, 
-           'SRC':spearman, 
-           'PCC':pearson, 
-           'MI':mutual_info_score, 
-           'MAE':mean_absolute_error}
-
-        scoring_clf = {'classifier_accuracy': accuracy_score, 
-           'classifier_f1': f1_score }
-
-        results_dir = '../reports/'
-
-        results = pd.DataFrame()
-
-        if not os.path.exists(results_dir):
-            os.makedirs(results_dir)
-
-        # Get model predictions
-        print('Testing the model... ')
-        # full predictions
-        predictions = pd.DataFrame()
-
+        # get model predictions
+        predictions = get_model_predictions(model,X)
         predictions['Y'] = Y #actual values
-        # print(model.predict(X))
-        Y_pred = np.reshape(model.predict(X),(-1,len(Y)))[0] #overall prediction
+        predictions['Y_binary'] = (Y != 0)
 
-        predictions['Y_pred'] = Y_pred
-
-        predictions['Y_pred_reg'] = np.nan
-        predictions['Y_pred_clas'] = np.nan
-        
-        try:
-            Y_pred_reg = model.reg.predict(X) # regressor predictions (combined models only)
-            predictions['Y_pred_reg'] = Y_pred_reg
-            Y_pred_clas = model.clas.predict(X) #classifier predictions (combined models only)
-            predictions['Y_pred_clas'] = Y_pred_clas
-            predictions['Y_pred_clas'] = predictions['Y_pred_clas'].astype(int)
-        except AttributeError:
-            pass
-
+        # score the predictions
         model_name = os.path.basename(model_path).replace('.model','')
-
-        #score the predictions
-        print('\nResults: ')
-        for name, metric in scoring_full.items():
-            score = np.round(metric(Y,Y_pred),2)
-            print('{}: {}'.format(name,score))
-            results.loc[model_name,name] = score
-
-        if not all(np.isnan(predictions['Y_pred_clas'])):
-            for name, metric in scoring_clf.items():
-                score = np.round(metric(Y_binary,predictions['Y_pred_clas']),2)
-                print('{}: {}'.format(name,score))
-                results.loc[model_name,name] = score
-        else:
-            for name, metric in scoring_clf.items():
-                score = np.nan
-                print('{}: {}'.format(name,score))
-                results.loc[model_name,name] = score
+        results = score_predictions(predictions,model_name)
 
         #store the results
         time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
@@ -127,8 +134,10 @@ def main(argv):
 
         results.to_csv(results_path)
         predictions.to_csv(predictions_path)
+
         print('\nResults saved to {}'.format(results_path))
         print('\nPredictions saved to {}'.format(predictions_path))
+
         return
 
 if __name__ == "__main__":
